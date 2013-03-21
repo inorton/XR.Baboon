1ac5c967836b29e75025ab13da8b266102c1ad76
Ian Norton <inorton@gmail.com>
2013-03-21 16:18:43 +0000

    yesss can highlight lines!

---
+++ cov-gtk/MainWindow.cs
@@ -0,0 +1,67 @@
+using System;
+using Gtk;
+using GtkSourceView;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		var manager = new SourceLanguageManager ();
+		SourceLanguage language = manager.GetLanguage ("c-sharp");
+
+		var buf = new SourceBuffer (language);
+
+		var tag = new TextTag ("green_bg");
+		tag.Background = "#00ff00";
+
+		buf.TagTable.Add (tag);
+
+		buf.HighlightSyntax = true;
+
+		var sv = new SourceView (buf);
+
+
+
+		var sw = new ScrolledWindow ();
+
+		buf.Text = @" 
+
+using System;
+
+public class Fooo {
+
+  public static void Run() {
+    Console.WriteLine(""go!"");
+  }
+
+}
+
+";
+
+
+
+		var iter1 = buf.GetIterAtLineOffset (7, 0);
+		var iter2 = buf.GetIterAtLineOffset (8, 0);
+		buf.ApplyTag (tag, iter1, iter2);
+
+
+
+
+		// buf.AddMark (new TextMark ("foo", true), iter);
+
+		sw.Add (sv);
+
+		this.Add (sw);
+
+		this.ShowAll ();
+
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
