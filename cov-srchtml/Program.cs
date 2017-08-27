ecf29e254cf3d5dd804fa842c7c6cf0c56fd349f
Richard Barnett <richard.barnett@nearmap.com>
2017-08-28 09:15:27 +1000

    cov-srchtml: restructure, use `frame` instead of `iframe`

--- cov-srchtml/Program.cs
+++ cov-srchtml/Program.cs
@@ -31,13 +31,113 @@ namespace covsrchtml
             coverageData.Open(args[0]);
             var codeRecords = coverageData.Load();
 
-            var lookup = codeRecords.ToLookup(x => x.SourceFile);
-
-            var srcDir = new DirectoryInfo(args[1]).FullName;
-
             var dirNames = new HashSet<string>();
             var fileNodes = new List<string>();
 
+            GenerateCoverageColourisedSources(
+                new DirectoryInfo(args[1]).FullName, 
+                outputDir, 
+                codeRecords.ToLookup(x => x.SourceFile), 
+                dirNames, 
+                fileNodes);
+            
+            GenerateSourceTree(outputDir, CreateDirNodeJson(dirNames), fileNodes);
+            GenerateIndex(outputDir);
+            
+            coverageData.Close();
+        }
+
+        private static void GenerateIndex(string outputDir)
+        {
+            using (var writer = new StreamWriter(outputDir + "/index.html"))
+            {
+                writer.WriteLine(
+                    @"<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<title>Coverage Report</title>
+</head>
+<frameset cols='20%,80%'>
+<frame src='tree.html' name='treeFrame' title='Source files'></frame>
+<frame name='sourceFrame'></frame>
+</frameset>
+</html>
+");
+            }
+        }
+
+        private static void GenerateSourceTree(string outputDir, IEnumerable<string> dirNodes, IEnumerable<string> fileNodes)
+        {
+            using (var writer = new StreamWriter(outputDir + "/tree.html"))
+            {
+                writer.WriteLine(
+                    @"<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.4/themes/default/style.min.css' />
+<style>.nm-code{font-family:sans-serif;font-size:80%;}</style>
+</head>
+<body>
+<div class='nm-code' style='vertical-align:top' id='jstree'></div>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.4/jstree.min.js'></script>
+<script>
+  $(function () {
+    $('#jstree').jstree({ 'core' : {
+      'multiple': false,
+      'animation': 0,
+      'data' : [
+"
+                    + string.Join(",\n", dirNodes) + ",\n"
+                    + string.Join(",\n", fileNodes) + @"
+      ]
+    }});
+  });
+$('#jstree').on('changed.jstree', function (e, data) {
+  console.log(data.selected);
+  if (data.selected[0].endsWith('.cs')) {
+    parent.sourceFrame.location.href=data.selected[0].substring(1) + '.html';
+  }
+});
+</script>
+</body>
+</html>");
+            }
+        }
+
+        private static IEnumerable<string> CreateDirNodeJson(ISet<string> dirs)
+        {
+            var nodes = new List<string>();
+            foreach (var dir in dirs.ToList())
+            {
+                string parent = null;
+                var path = "";
+
+                foreach (var comp in dir.Split('/').Skip(1))
+                {
+                    path += $"/{comp}";
+                    if (path == dir || dirs.Add(path))
+                    {
+                        nodes.Add(String.Format(
+                                "{{'id':'{0}','parent':'{1}','text':'{2}','state':{{'opened':true}}}}", path, parent ?? "#",
+                                comp)
+                            .Replace('\'', '"'));
+                    }
+                    parent = path;
+                }
+            }
+            return nodes;
+        }
+
+        private static void GenerateCoverageColourisedSources(
+            string srcDir, 
+            string outputDir, 
+            ILookup<string, CodeRecord> codeRecordLookup, 
+            ISet<string> dirNames,
+            ICollection<string> fileNodes)
+        {
             var srcFiles = new DirectoryInfo(srcDir).EnumerateFiles("*.cs", SearchOption.AllDirectories);
             foreach (var srcFileInfo in srcFiles)
             {
@@ -55,101 +155,19 @@ namespace covsrchtml
                 var relDirName = dirName.Substring(srcDir.Length);
 
                 var covFile = outputDir + relSrcFile + ".html";
-                var fileRecords = lookup[srcFileName].ToList();
+                var fileRecords = codeRecordLookup[srcFileName].ToList();
                 GenerateCoverageColourisedFile(srcFileName, relSrcFile, covFile, fileRecords);
-
-                if (srcFileName.EndsWith("/ZuoraRestApi.cs") || srcFileName.EndsWith("/ZuoraApi.cs"))
-                {
-                    Console.WriteLine(srcFileName);
-                    foreach (var rec in fileRecords)
-                    {
-                        Console.WriteLine(rec);
-                        Console.WriteLine("lines [" + string.Join(",", rec.GetLines()) + "]");
-                        Console.WriteLine("hits  [" + string.Join(",", rec.GetHitCounts()) + "]");
-                    }
-                    Console.WriteLine("all lines [" + string.Join(",", fileRecords.SelectMany(x => x.GetLines())) + "]");
-                    Console.WriteLine();
-                }
 
                 var totalLines = fileRecords.SelectMany(x => x.GetLines()).Distinct().Count();
                 var coveredLines = fileRecords.SelectMany(x => x.GetHitCounts().Keys).Distinct().Count();
                 var covPct = totalLines == 0 ? 0 : 100 * coveredLines / totalLines;
-                
+
                 dirNames.Add(relDirName);
                 fileNodes.Add(String.Format(
-                        "{{'id':'{0}','parent':'{1}','text':'{2} ({3}%)'}}", 
+                        "{{'id':'{0}','parent':'{1}','text':'{2} ({3}%)'}}",
                         relSrcFile, relDirName, srcFileInfo.Name, covPct)
                     .Replace('\'', '"'));
             }
-
-            var dirNodes = new List<string>();
-            foreach (var dir in dirNames.ToList())
-            {
-                string parent = null;
-                var path = "";
-
-                foreach (var comp in dir.Split('/').Skip(1))
-                {
-                    path += $"/{comp}";
-                    if (path == dir || dirNames.Add(path))
-                    {
-                        dirNodes.Add(String.Format(
-                                "{{'id':'{0}','parent':'{1}','text':'{2}','state':{{'opened':true}}}}", path, parent ?? "#", comp)
-                            .Replace('\'', '"'));
-                    }
-                    parent = path;
-                }
-            }
-
-            using (var writer = new StreamWriter(outputDir + "/tree.html"))
-            {
-                writer.WriteLine(
-                    @"<!DOCTYPE html>
-<html>
-<head>
-<meta charset='utf-8'>
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.4/themes/default/style.min.css' />
-<style>.nm-code{font-family:sans-serif;font-size:80%;}</style>
-</head>
-<body>
-<table>
-<tr>
-<td class='nm-code' style='vertical-align:top'>
-<div id='jstree'></div>
-</td>
-<td style='width:80%;vertical-align:top'>
-<iframe id='cov-code' style='width:100%'><p>Code goes here</p></iframe>
-</td>
-</tr>
-</table>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.4/jstree.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js'></script>
-<script>
-  iFrameResize({log:true}, '#cov-code');
-  $(function () {
-    $('#jstree').jstree({ 'core' : {
-      'multiple': false,
-      'animation': 0,
-      'data' : [
-"
-                    + string.Join(",\n", dirNodes) + ",\n"
-                    + string.Join(",\n", fileNodes) + @"
-      ]
-    }});
-  });
-$('#jstree').on('changed.jstree', function (e, data) {
-  console.log(data.selected);
-  if (data.selected[0].endsWith('.cs')) {
-    $('#cov-code').attr('src', data.selected[0].substring(1) + '.html');
-  }
-});
-</script>
-</body>
-</html>");
-            }
-
-            coverageData.Close();
         }
 
         private static void GenerateCoverageColourisedFile(string srcFile, string relSrcFile, string outFile,
@@ -163,7 +181,6 @@ $('#jstree').on('changed.jstree', function (e, data) {
                 writer.WriteLine("<!DOCTYPE html>");
                 writer.WriteLine($"<html>\n<head><title>{relSrcFile}</title></head>");
                 writer.WriteLine(SrcHtmlStyle);
-                writer.WriteLine("<script src='https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.contentWindow.min.js'></script>");
                 writer.WriteLine("<body>\n<table cellspacing='0' cellpadding='0'>");
                 
                 var n = 0;
@@ -196,7 +213,7 @@ $('#jstree').on('changed.jstree', function (e, data) {
 
         static void Usage()
         {
-            Console.Error.WriteLine("Usage: cov-html COVERAGEDB SRCDIR OUTPUTDIR");
+            Console.Error.WriteLine("Usage: cov-srchtml COVERAGEDB SRCDIR OUTPUTDIR");
         }
         
         private const string SrcHtmlStyle = @"
